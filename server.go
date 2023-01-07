1e721b03fae1823f57a860d65f60416767afd009
yuanxi <1666739696>
2023-01-07 20:11:26 +0800

    unfixed

--- server.go
+++ server.go
@@ -3,12 +3,11 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"github.com/darabuchi/log"
 	"io"
 	"net/http"
 	"reflect"
 	"sync"
-	
-	"github.com/darabuchi/log"
 )
 
 type Ctx struct {
@@ -17,40 +16,30 @@ type Ctx struct {
 }
 
 type Handler struct {
-	mapper map[string]interface{}
-	lock   sync.RWMutex
+	trie *Trie
+	lock sync.RWMutex
 }
 
 func NewHandler() *Handler {
 	p := &Handler{
-		mapper: map[string]interface{}{},
+		trie: new(Trie),
 	}
-	
+
 	return p
 }
 
-func (p *Handler) MapPathToFunc(path string, s interface{}) {
-	p.lock.Lock()
-	if _, ok := p.mapper[path]; !ok {
-		p.mapper[path] = s
-	}
-	p.lock.Unlock()
-}
-
-func (p *Handler) Register(path string, logic interface{}) {
-	// 校验logic是否是一个函数，并且函数的入参和出参是否否则规范
-	// 同时记录path对应的logic
-	
+func NewAnalyze(logic interface{}) Analysis {
 	s := reflect.TypeOf(logic)
 	if s.Kind() != reflect.Func {
 		panic("parameter is not func")
 	}
-	
+	var analysis Analysis
+	analysis.fun = logic
+
 	if s.NumIn() != 2 {
 		panic("num of func in is not 2")
 	}
-	
-	x := s.In(0).Elem()
+	x := s.In(0)
 	for x.Kind() == reflect.Ptr {
 		x = x.Elem()
 	}
@@ -60,72 +49,96 @@ func (p *Handler) Register(path string, logic interface{}) {
 	if x.Name() != "Ctx" {
 		panic("first in is must *Ctx")
 	}
-	
-	x = s.In(1).Elem()
+	analysis.in = append(analysis.in, x)
+
+	x = s.In(1)
 	for x.Kind() == reflect.Ptr {
 		x = x.Elem()
 	}
 	if x.Kind() != reflect.Struct {
 		panic("second in is must struct")
 	}
-	
+	analysis.in = append(analysis.in, x)
+
 	if s.NumOut() != 2 {
 		panic("num of func in is not 2")
 	}
-	x = s.Out(0).Elem()
+	x = s.Out(0)
 	for x.Kind() == reflect.Ptr {
 		x = x.Elem()
 	}
 	if x.Kind() != reflect.Struct {
 		panic("outFirst in is must struct")
 	}
-	
+	analysis.out = append(analysis.out, x)
+
 	x = s.Out(1)
 	if x.Name() != "error" {
 		panic("outSecond in is must error")
 	}
-	
-	p.MapPathToFunc(path, s)
+	analysis.out = append(analysis.out, x)
+
+	return analysis
+
+}
+
+func (p *Handler) Register(path string, logic interface{}) {
+	// 校验logic是否是一个函数，并且函数的入参和出参是否规范
+	// 同时记录path对应的logic
+	analysis := NewAnalyze(logic)
+	p.trie.Insert(path, &analysis)
 }
 
 func (p *Handler) Call(writer http.ResponseWriter, request *http.Request) error {
 	// 根据request的path，找到对应的logic，并且调用
-	logic, ok := p.mapper[request.URL.Path]
-	if !ok {
-		return errors.New("Path is unregistered")
+	node, err := p.trie.Find(request.URL.Path)
+	if err != nil {
+		log.Errorf("err:%v", err)
+		return err
 	}
-	s := reflect.TypeOf(logic)
-	in1 := s.In(0).Elem()
-	in2 := s.In(1).Elem()
-	log.Info(in2)
-	
+
+	var logic *Analysis
+	for _, v := range node.childrenLogic {
+		logic = v.logic
+	}
+
 	buf, err := io.ReadAll(request.Body)
 	if err != nil {
 		log.Errorf("err:%v", err)
 		return err
 	}
-	
-	err = json.Unmarshal(buf, in2)
-	log.Info(in2)
+
+	arg2 := reflect.New(logic.in[1])
+
+	err = json.Unmarshal(buf, arg2.Interface())
+
 	if err != nil {
 		log.Errorf("err:%v", err)
 		return err
 	}
-	
-	call := reflect.New(s).Call([]reflect.Value{reflect.ValueOf(in1), reflect.ValueOf(in2)})
-	log.Info(call)
-	resp, err := json.Marshal(call[0].Interface())
-	if err != nil {
+
+	v := reflect.ValueOf(logic.fun)
+	arg1 := &Ctx{
+		writer:  writer,
+		request: request,
+	}
+	call := v.Call([]reflect.Value{reflect.ValueOf(arg1), arg2})
+
+	resp, err1 := json.Marshal(call[0].Interface())
+	if err1 != nil {
 		log.Errorf("err:%v", err)
 		return err
 	}
-	
+	if call[1].Interface() != nil {
+		return call[1].Interface().(error)
+	}
+
 	_, err = writer.Write(resp)
 	if err != nil {
 		log.Errorf("err:%v", err)
 		return err
 	}
-	
+
 	return nil
 }
 
@@ -136,33 +149,113 @@ func (p *Handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
+func NewTrieNode(char string, analysis *Analysis) *trieNode {
+	return &trieNode{
+		char:          char,
+		logic:         analysis,
+		isEnding:      false,
+		children:      make(map[rune]*trieNode),
+		childrenLogic: make(map[*Analysis]*trieNode),
+	}
+}
+
+func (p *Handler) NewTrie() *Trie {
+	trieNode := NewTrieNode("/", nil)
+	return &Trie{trieNode}
+}
+
+func (t *Trie) Insert(word string, analysis *Analysis) error {
+	node := t.root
+	for _, code := range word {
+		value, ok := node.children[code]
+		if !ok {
+			value = NewTrieNode(string(code), nil)
+			node.children[code] = value
+		}
+		node = value
+	}
+	value, ok := node.childrenLogic[analysis]
+	if !ok {
+		if !ok && len(node.childrenLogic) == 0 {
+			value = NewTrieNode("", analysis)
+			node.childrenLogic[analysis] = value
+		} else {
+			errors.New("logic is wrong")
+		}
+	}
+	node = value
+	node.isEnding = true
+	return nil
+}
+
+func (t *Trie) Find(word string) (*trieNode, error) {
+	node := t.root
+	for _, code := range word {
+		value, ok := node.children[code]
+		if !ok {
+			return nil, errors.New("Path is not unRegistered")
+		}
+		node = value
+	}
+	return node, nil
+}
+
+type Analysis struct {
+	intNum int
+	outNum int
+	in     []reflect.Type
+	out    []reflect.Type
+	fun    interface{}
+}
+
 type (
 	GetUserReq struct {
+		A string `json:"a"`
+		B string `json:"b"`
 	}
-	
+
 	GetUserRsp struct {
+		A string `json:"a"`
+		B string `json:"b"`
 	}
 )
 
 type (
 	SetUserReq struct {
+		C string `json:"c"`
+		D string `json:"d"`
 	}
-	
+
 	SetUserRsp struct {
+		C string `json:"c"`
+		D string `json:"d"`
+	}
+)
+
+type (
+	trieNode struct {
+		char          string
+		logic         *Analysis
+		isEnding      bool
+		children      map[rune]*trieNode
+		childrenLogic map[*Analysis]*trieNode
+	}
+	Trie struct {
+		root *trieNode
 	}
 )
 
 func main() {
 	mux := NewHandler()
 	mux.Register("/GetUser", func(ctx *Ctx, req *GetUserReq) (*GetUserRsp, error) {
-		
-		return nil, errors.New("not handle")
+
+		return &GetUserRsp{}, nil
 	})
-	
+
 	mux.Register("/SetUser", func(ctx *Ctx, req *SetUserReq) (*SetUserRsp, error) {
-		
+
 		return nil, errors.New("not handle")
 	})
-	
+
 	_ = http.ListenAndServe(":8080", mux)
 }
