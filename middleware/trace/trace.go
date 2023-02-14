66886a98bf94c19fe34014e0cb97433cd84a70ff
落心 <luoxin.ttt@gmial.com>
2023-02-14 15:02:48 +0800

    traceid

--- middleware/trace/trace.go
+++ middleware/trace/trace.go
@@ -8,6 +8,9 @@ import (
 type Option struct {
 	// 读trace id的header，如果不存在，则使用 `X-Goon-Trace` 作为header
 	Header string `json:"header,omitempty"`
+
+	// 生成trace id的函数
+	GenTraceId func() string `json:"trace_id_func,omitempty"`
 }
 
 func Handler(opt Option) func(ctx *goon.Ctx) error {
@@ -15,10 +18,14 @@ func Handler(opt Option) func(ctx *goon.Ctx) error {
 		opt.Header = "X-Goon-Trace"
 	}
 
+	if opt.GenTraceId == nil {
+		opt.GenTraceId = log.GenTraceId
+	}
+
 	return func(ctx *goon.Ctx) error {
 		traceId := ctx.GetReqHeader(opt.Header)
 		if traceId == "" {
-			traceId = log.GenTraceId()
+			traceId = opt.GenTraceId()
 		}
 		log.SetTrace(traceId)
 		defer log.DelTrace()
