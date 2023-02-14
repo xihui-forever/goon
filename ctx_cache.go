40492004c50264f4a2597931cfc9b00e33b3eea1
落心 <luoxin.ttt@gmial.com>
2023-02-14 14:55:27 +0800

    cache

--- ctx_cache.go
+++ ctx_cache.go
@@ -1,1 +1,13 @@
 package goon
+
+func (p *Ctx) Cache() *Cache {
+	return p.cache
+}
+
+func (p *Ctx) Get(key string) (interface{}, bool) {
+	return p.cache.Get(key)
+}
+
+func (p *Ctx) Set(key string, value interface{}) {
+	p.cache.Set(key, value)
+}
