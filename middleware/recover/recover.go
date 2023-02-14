cdab798d2c010f28ee4c701972fba39cb658a870
落心 <luoxin.ttt@gmial.com>
2023-02-14 14:59:22 +0800

    recover

---
+++ middleware/recover/recover.go
@@ -0,0 +1,26 @@
+package recover
+
+import (
+	"github.com/darabuchi/log"
+	"github.com/xihui-forever/goon"
+)
+
+type Option struct {
+	Next func(ctx *goon.Ctx) error
+}
+
+func Handler(opt Option) func(ctx *goon.Ctx) error {
+	return func(ctx *goon.Ctx) (err error) {
+		defer func() {
+			// 接收panic的信息，防止某一个请求导致程序崩溃
+			if e := recover(); e != nil {
+				log.Errorf("PANIC err:%v", e)
+				if opt.Next != nil {
+					err = opt.Next(ctx)
+				}
+			}
+		}()
+
+		return ctx.Next()
+	}
+}
